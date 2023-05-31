6c7d6d511b74cc07c8db4ccf5283a1b2ab95253d
Simon-XzMC <simonlblakemore@gmail.com>
2023-05-31 12:35:58 +0100

    Testing

--- src/main/java/com/simonxz/plugin/Plugin.java
+++ src/main/java/com/simonxz/plugin/Plugin.java
@@ -8,6 +8,8 @@ public final class Plugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Plugin has been started!");
+        // testing
+
     }
 
     @Override
