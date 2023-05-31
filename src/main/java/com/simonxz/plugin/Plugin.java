bdf9128da3c41de71d4e86c3e1bbf9151b4c39d6
Simon-XzMC <simonlblakemore@gmail.com>
2023-05-31 14:54:37 +0100

    Frozen & Egg throw event

--- src/main/java/com/simonxz/plugin/Plugin.java
+++ src/main/java/com/simonxz/plugin/Plugin.java
@@ -1,18 +1,38 @@
 package com.simonxz.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Plugin extends JavaPlugin {
+public final class Plugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Plugin has been started!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "Stop moving, you are frozen");
+
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
+
+        e.getPlayer().sendMessage(ChatColor.AQUA + "You just threw an egg!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("Plugin has been disabled!");
+
     }
 }
