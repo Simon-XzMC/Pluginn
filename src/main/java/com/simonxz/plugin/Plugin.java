e27e84f7fc4c415cb5658d65d99ce74b651d19f2
Simon-XzMC <simonlblakemore@gmail.com>
2023-05-31 12:29:33 +0100

    Testing

---
+++ src/main/java/com/simonxz/plugin/Plugin.java
@@ -0,0 +1,18 @@
+package com.simonxz.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Plugin has been started!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("Plugin has been disabled!");
+    }
+}
