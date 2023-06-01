0c020b0d11ff7b63409f32ae613c8c50d5fd6859
Simon-XzMC <simonlblakemore@gmail.com>
2023-06-01 07:56:37 +0100

    Config

--- src/main/java/com/simonxz/plugin/ConfigCommand.java
+++ src/main/java/com/simonxz/plugin/ConfigCommand.java
@@ -5,6 +5,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class ConfigCommand implements CommandExecutor {
 
     private Plugin plugin;
@@ -17,6 +19,17 @@ public class ConfigCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
         if (commandSender instanceof Player) {
+
+
+            // Setting values to config when command runs
+            plugin.getConfig().set("Word", "Cake");
+            List<String> list = plugin.getConfig().getStringList("String-List");
+            list.add("New Value!");
+            plugin.getConfig().set("String-List", list);
+            plugin.saveConfig();
+
+
+            // reading values from config when command runs
             Player player = (Player) commandSender;
             player.sendMessage(plugin.getConfig().getString("Word"));
             player.sendMessage(plugin.getConfig().getInt("Number" ) + "");
